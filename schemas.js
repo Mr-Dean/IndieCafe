c7a920934298c2337bb28a3cdf9e9703e1ac7bba
Mr-Dean <deanarnold0@gmail.com>
2021-07-21 18:04:01 +0100

    fixed typo on schema

--- schemas.js
+++ schemas.js
@@ -3,8 +3,8 @@ const Joi = require('joi');
 module.exports.cafeSchema = Joi.object({
     cafe: Joi.object({
         name: Joi.string().required(),
-        address: Joi.string().required,
         image: Joi.string().required(),
+        address: Joi.string().required(),
         description: Joi.string().required()
     }).required()
 });
