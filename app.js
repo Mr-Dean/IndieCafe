aa49f521235abbf7ae815827032cf5d0af62e980
Mr-Dean <deanarnold0@gmail.com>
2021-08-29 14:35:51 +0100

    created public folfer and added form validation to it

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ db.once("open", () => {
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.engine('ejs', ejsMate);
