06daf1c25e7c7008ef084c61ae017425c1c3c9df
Mr-Dean <deanarnold0@gmail.com>
2021-08-30 12:37:36 +0100

    setup passport on app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const PassportLocal = require('passport-local');
+const User = require('./models/user');
 
 
 const cafes = require('./routes/cafes');
@@ -44,6 +47,13 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new PassportLocal(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
