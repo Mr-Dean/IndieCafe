062645ff9d39efa1a17f3e0dd1a2b6625ea6788a
Mr-Dean <deanarnold0@gmail.com>
2021-09-01 19:48:20 +0100

    fixed bug in authorization, forgot to call next() in isAuthor and isReviewAuthor

--- middleware.js
+++ middleware.js
@@ -21,15 +21,17 @@ module.exports.isAuthor = async(req, res, next) => {
         req.flash('error', 'You do not have permission to do that');
         return res.redirect(`/cafes/${id}`);
     }
+    next()
 }
 
 module.exports.isReviewAuthor = async(req, res, next) => {
     const { id, reviewId } = req.params;
-    const review = await Review.findById(id);
-    if(!review.author.equals(reviewId)) {
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that');
         return res.redirect(`/cafes/${id}`);
     }
+    next()
 }
 
 module.exports.validateReview = (req, res, next) => {
