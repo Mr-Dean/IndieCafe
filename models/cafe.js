faabb29d7804e106479be8ceb0465c43616d8d3e
Mr-Dean <deanarnold0@gmail.com>
2021-10-05 18:49:02 +0100

    created a virtual to display image thumbnails using cloudinary transformation api

--- models/cafe.js
+++ models/cafe.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CafeSchema = new Schema({
     name: String,
     description: String,
     address: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
