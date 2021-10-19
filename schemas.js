5a8ba4a434cd3f1793baf9794e3a03e257c88af2
Mr-Dean <deanarnold0@gmail.com>
2021-10-19 18:03:03 +0100

    installed helmet + content security policies

--- schemas.js
+++ schemas.js
@@ -6,19 +6,22 @@ const extension = (joi) => ({
     type: 'string',
     base: joi.string(),
     messages: {
-        'string.escapeHTML' : '{{#label}} must not include HTML!'
+        'string.escapeHTML': '{{#label}} must not include HTML!'
     },
     rules: {
-        validate(value, helpers) {
-            const clean = sanitizeHtml(value, {
-                allowedTags: [],
-                allowedAttributes: {},
-            });
-            if (clean !== value) return helpers.error('string.escapeHTML', {value})
-            return clean;
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
         }
     }
 });
+
 
 const Joi = BaseJoi.extend(extension);
 
