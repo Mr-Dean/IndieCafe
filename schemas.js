cba6b3ccbd5dedf059ac7290a03f6b862cc988b2
Mr-Dean <deanarnold0@gmail.com>
2021-10-19 17:24:36 +0100

    installed sanitize-html and added joi extension to clean html in form inputs

--- schemas.js
+++ schemas.js
@@ -1,11 +1,33 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML' : '{{#label}} must not include HTML!'
+    },
+    rules: {
+        validate(value, helpers) {
+            const clean = sanitizeHtml(value, {
+                allowedTags: [],
+                allowedAttributes: {},
+            });
+            if (clean !== value) return helpers.error('string.escapeHTML', {value})
+            return clean;
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.cafeSchema = Joi.object({
     cafe: Joi.object({
-        name: Joi.string().required(),
+        name: Joi.string().required().escapeHTML(),
         //image: Joi.string().required(),
-        address: Joi.string().required(),
-        description: Joi.string().required()
+        address: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
@@ -13,6 +35,6 @@ module.exports.cafeSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required(),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHTML()
     }).required()
 })
